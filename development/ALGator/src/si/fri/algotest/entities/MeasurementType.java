ec32cf2f9fa2197d01233e3538e297024b49bb3f
Tomaž Dobravec <tomaz.dobravec@fri.uni-lj.si>
2015-05-12 13:31:33 +0200

    .

--- development/ALGator/src/si/fri/algotest/entities/MeasurementType.java
+++ development/ALGator/src/si/fri/algotest/entities/MeasurementType.java
@@ -1,36 +1,28 @@
 package si.fri.algotest.entities;
 
-import java.io.Serializable;
-
 /**
  *
  * @author tomaz
  */
-public enum MeasurementType implements Serializable {
-  EM {
-    @Override
-    public String toString() {
-      return "Regular mesurement of parameters and timers";
-    }
-    
-  }, 
-  
-  JVM {
-    @Override
-    public String toString() {
-      return "Measurement of JVM parameters";
-    }
-    
-  }, 
-  
-  CNT {
-    @Override
-    public String toString() {
-      return "Measurement of user defined counters";
-    }
-    
-  };
+public enum MeasurementType  {
+  EM, JVM, CNT;
 
+  @Override
+  public String toString() {
+    switch (this) {
+      case EM:
+        return "Regular mesurement of parameters and timers"; 
+      case JVM:
+        return "Measurement of JVM parameters";
+      case CNT:
+        return "Measurement of user defined counters";
+      default:
+        return "/unknown/";
+    }
+  }
+
+  
+  
   /**
    * Returns the extension to the result file for a  measurement of a given type
    * (results/AlgName-TestSetname.extension)
